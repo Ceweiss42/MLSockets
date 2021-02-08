af7fa3f2bf509d6a60958fb0f792231849fec8ab
Ceweiss42 <42663413+Ceweiss42@users.noreply.github.com>
2021-02-07 20:04:39 -0800

    Update clientConnect.py

--- clientConnect.py
+++ clientConnect.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import load_model
 model = load_model(r"C:\Users\cewei\Desktop\model.h5")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.4.85", 6969))
+s.connect(("#############", 6969))
 data = b""
 
 while True:
